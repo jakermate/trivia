ccfb644f6c6711c7a20695ea28e8c577afac2ccb
jakermate <jmcfly@gmail.com>
2019-03-23 08:07:23 -0700

    Formatting routes.
    Added route to handle caching of questions.

--- server/server.js
+++ server/server.js
@@ -7,15 +7,19 @@ const scoreModel = require('./score')
 const scores = {
     scores: "test score"
 }
+// startup
 
 app.listen(port, function(){
     console.log('Trivia score server listening on port ' + port)
 })
 
+// score fetching
 
 app.get('/topscores', function(req,res){
     res.send(scores)
 })
+
+// score tracking
 
 app.post('/addscore', function(req,res){
     // post new score and rank in the score cache
@@ -23,6 +27,12 @@ app.post('/addscore', function(req,res){
     let user = req.userID
 })
 
+
+// question caching
+
+app.post('/addquestions', function(req,res){
+    console.log("Ready to parse received question set: " + req.body.questions+" Sent by user " + req.body.userID)
+})
 
 function updateCache(){
     
