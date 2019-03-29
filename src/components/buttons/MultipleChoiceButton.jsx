8f954a3b2e487e9e23a7d5c34a3055e55bc9ddd3
jakermate <jmcfly@gmail.com>
2019-03-29 12:13:30 -0700

    Component that receives answer prop to be used for multiple choice questions via the map function.

---
+++ src/components/buttons/MultipleChoiceButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function MultipleChoiceButton(props) {
+    return (
+        <MultipleChoiceButtonContainer>
+            {this.props.answer}
+        </MultipleChoiceButtonContainer>
+    )
+}
+
+const MultipleChoiceButtonContainer = styled.button`
+
+`
