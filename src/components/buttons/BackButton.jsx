9f9dcd3a5117944bbd5a0e6779d4c7cfe6cd8b61
jakermate <jmcfly@gmail.com>
2019-03-30 08:02:21 -0700

    Now says BACK

--- src/components/buttons/BackButton.jsx
+++ src/components/buttons/BackButton.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 export default function BackButton() {
     return (
         <BackButtonContainer>
-        
+            BACK
         </BackButtonContainer>
     )
 }
