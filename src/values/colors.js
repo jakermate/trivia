4f6c7a957ed9d85eb15ae863194a847ba4c08a51
jakermate <jmcfly@gmail.com>
2019-04-07 12:49:27 -0700

    Values directory added.
    Colors object added with primary app theme values.

---
+++ src/values/colors.js
@@ -0,0 +1,8 @@
+export default {
+    primaryLight: '#00D1AE',
+    primaryDark: '',
+    secondaryLight: '#00B3CC',
+    secondaryDark: '',
+    backgroundPrimary: '',
+    backgroundSecondary: ''
+}
