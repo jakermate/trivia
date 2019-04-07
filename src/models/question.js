8d911e512c2f628fd9bd324329500643a246df23
jakermate <jmcfly@gmail.com>
2019-04-07 12:50:16 -0700

    Placeholder values for selected answers changed from integer 0 to null value.

--- src/models/question.js
+++ src/models/question.js
@@ -2,7 +2,7 @@ export class questionTrueFalse{
         constructor(questionString, difficulty, category, correctAnswer){
             this.questionString= questionString
             this.correctAnswer= correctAnswer
-            this.selectedAnswer= 0 // temp int 0 until used
+            this.selectedAnswer= null // temp int 0 until used
             this.difficulty= difficulty
             this.category=category
             this.type = "boolean"
@@ -14,7 +14,7 @@ export class questionMultipleChoice{
         this.questionString= questionString
         this.answers= answers
         this.correctAnswer= this.answers.push(correctAnswer)
-        this.selectedAnswer= 0 // set to integer 0 as placeholder until used
+        this.selectedAnswer= null // set to integer 0 as placeholder until used
         this.difficulty= difficulty
         this.category=category
         this.type = "multiple"
