95a5bedceb9f5fbf92850668f819d04ee7d4a9d2
jakermate <jmcfly@gmail.com>
2019-04-07 12:50:49 -0700

    Footer fleshed out with copyright. (Unsure of necessity of component.)

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
+import logo from '../../img/shield.png'
 
 export default function Footer() {
     return (
-        <Footer>
+        <FooterContainer>
             <Content>
-                Jake Miller &copy;
+                <LogoContainer>
+                    <img height="100" src={logo} alt=""/>
+                </LogoContainer>
+                <Copyright>
+                    Jake Miller &copy;
+                </Copyright>
             </Content>
-        </Footer>
+        </FooterContainer>
   )
 }
 
-const Footer = styled.div`
+const FooterContainer = styled.div`
     height: 100px;
     padding: 1rem 2rem;
+    width: 100%;
+    box-sizing: border-box;
 `
+
+const LogoContainer = styled.div`
+    height: 60px;
+`
+
 
 const Content = styled.div`
     margin: 0 auto;
